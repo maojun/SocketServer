515e8671840357a9f771d1accaeeb769fc84caf2
毛军 <jun-mao@gable-tech.com.cn>
2017-05-27 17:36:17 +0800

    qw

--- SocketServer/src/main/java/com/gable/socket/thread/ReadSocketClientResult2.java
+++ SocketServer/src/main/java/com/gable/socket/thread/ReadSocketClientResult2.java
@@ -32,34 +32,9 @@ public class ReadSocketClientResult2 implements Runnable {
 	@Override
 	public void run() {
 		Socket socket = InitUtil.skMap.get(port);
-		SocketBean object = null;
 		if (socket != null) {
 			try {
-				// 轮询开始时间
-				long startWaitTime = System.currentTimeMillis();
-				// 轮询抓取客户端匹配信息
-				log.info("_____SocketServerServiceImpl3,socket端口:" + port + "开始抓取客户端返回结果,开始时间：" + startWaitTime
-						+ ",流水号:" + JsonUtil.toJsonString(uid));
-				//在限定时间类抓取到结果，放入结果集  InitUtil.resultMap
-				while (object == null && System.currentTimeMillis() - startWaitTime < MaxTime) {
-					InputStream in = socket.getInputStream();
-					if (in.available() > 0) {
-						ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-						Object obj = ois.readObject();
-						//跳过心跳包  "Heartbeat" 为心跳包发送过来的数据
-						if (obj != null && !obj.toString().equals("Heartbeat")) {
-							String jsonStr = JSON.toJSONString(obj);
-							object = JsonUtil.getObject(jsonStr, SocketBean.class);
-							//这里不再需要流水号判断
-							if (object != null) {
-								log.info("_____SocketServerServiceImpl4,流水号：" + JsonUtil.toJsonString(uid) + "返回结果："
-										+ JsonUtil.toJsonString(object));
-								//将客户端返回的结果统一放到结果集中
-								InitUtil.resultMap.put(object.getUid(), object);
-							}
-						}
-					}
-				}
+				insertData(socket);				
 			} catch (Exception e) {
 				log.error("_____SocketServerServiceImpl5,流水号：" + JsonUtil.toJsonString(uid) + "抓取客户端返回结果异常："
 						+ e.toString());
@@ -73,6 +48,41 @@ public class ReadSocketClientResult2 implements Runnable {
 			}
 			log.info("_____SocketServerServiceImpl6,流水号：" + JsonUtil.toJsonString(uid) + "抓取客户端超时,结束时间："
 					+ System.currentTimeMillis());
+		}
+	}
+	
+	private synchronized void insertData(Socket socket){
+		// 轮询开始时间
+		long startWaitTime = System.currentTimeMillis();
+		// 轮询抓取客户端匹配信息
+		log.info("_____SocketServerServiceImpl3,socket端口:" + port + "开始抓取客户端返回结果,开始时间：" + startWaitTime
+				+ ",流水号:" + JsonUtil.toJsonString(uid));
+		//在限定时间类抓取到结果，放入结果集  InitUtil.resultMap
+		SocketBean object = null;
+		while (object == null && System.currentTimeMillis() - startWaitTime < MaxTime) {
+			ObjectInputStream ois = null;
+			try{
+				InputStream in = socket.getInputStream();
+				if (in.available() > 0) {
+					ois = new ObjectInputStream(socket.getInputStream());
+					Object obj = ois.readObject();
+					//跳过心跳包  "Heartbeat" 为心跳包发送过来的数据
+					if (obj != null && !obj.toString().equals("Heartbeat")) {
+						String jsonStr = JSON.toJSONString(obj);
+						object = JsonUtil.getObject(jsonStr, SocketBean.class);
+						//这里不再需要流水号判断
+						if (object != null) {
+							log.info("_____SocketServerServiceImpl4,流水号：" + JsonUtil.toJsonString(uid) + "返回结果："
+									+ JsonUtil.toJsonString(object));
+							//将客户端返回的结果统一放到结果集中
+							InitUtil.resultMap.put(object.getUid(), object);
+						}
+					}
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
 		}
 	}
 }
